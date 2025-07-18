2dc504b6cbd4f19bfcd53fa53924ef87b8f1fa49
Khlivniuk Oleksandr <sasha2056083@gmail.com>
2025-07-18 18:25:29 +0300

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,1 +1,79 @@
-# write serializers here
+from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+
+from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Genre
+        fields = ("id", "name")
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+    class Meta:
+        model = Actor
+        fields = ("id", "first_name", "last_name", "full_name")
+
+    def get_full_name(self, obj):
+        return obj.full_name()
+
+class CinemaHallSerializer(serializers.ModelSerializer):
+    capacity = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CinemaHall
+        fields = ("name", "rows", "seats_in_row", "capacity")
+
+    def get_capacity(self, obj):
+        return obj.capacity
+
+
+class MovieSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Movie
+        fields = ("title", "description", "duration", "genres", "actors")
+
+
+class MovieRetrieveSerializer(MovieSerializer):
+    genres = GenreSerializer(many=True)
+    actors = ActorSerializer(many=True)
+
+
+class MovieListSerializer(MovieSerializer):
+    genres = SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field="name"
+    )
+    actors = serializers.StringRelatedField(many=True)
+
+
+class MovieSessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MovieSession
+        fields = ("id", "show_time", "movie", "cinema_hall")
+
+
+class MovieSessionRetrieveSerializer(MovieSessionSerializer):
+    movie = MovieListSerializer(many=False)
+    cinema_hall = CinemaHallSerializer(many=False)
+
+
+class MovieSessionListSerializer(MovieSessionSerializer):
+    movie_title = serializers.CharField(source="movie.title", read_only=True)
+    cinema_hall_name = serializers.CharField(source="cinema_hall.name", read_only=True)
+    capacity = serializers.IntegerField(source="cinema_hall.capacity", read_only=True)
+
+    class Meta:
+        model = MovieSession
+        fields = [
+            "id",
+            "show_time",
+            "movie_title",
+            "cinema_hall_name",
+            "capacity"
+        ]
--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,20 @@
-# write urls here
+
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from cinema.views import ActorViewSet, GenreViewSet, CinemaHallViewSet, MovieViewSet, MovieSessionViewSet
+
+app_name = 'cinema'
+
+router = DefaultRouter()
+
+router.register("actors", ActorViewSet)
+router.register("genres", GenreViewSet)
+router.register("cinema_halls", CinemaHallViewSet)
+router.register("movies", MovieViewSet)
+router.register("movie_sessions", MovieSessionViewSet, basename="movie-sessions")
+
+urlpatterns = [
+    path("", include(router.urls))
+]
